9c4afd35eb1e237c5ef0df0befeabd65afb98c05
phamduyhk <phamduyhk@gmail.com>
2020-03-29 22:31:59 +0900

    fix

--- assemble/TransformerTraining.py
+++ assemble/TransformerTraining.py
@@ -13,7 +13,7 @@ import datetime
 import os
 import sys
 sys.path.insert(0, os.path.abspath(
-    os.path.join(os.path.dirname(__file__), './utils')))
+    os.path.join(os.path.dirname(__file__), '../utils')))
 
 from EarlyStopping import EarlyStopping
 from transformer import TransformerClassification
