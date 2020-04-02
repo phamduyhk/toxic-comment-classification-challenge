fbd925786fa24ade242e6f3b9bc1cc2b12c1a68e
phamduyhk <phamduyhk@gmail.com>
2020-04-02 23:28:23 +0900

    fix predict

--- predict_xlnet.py
+++ predict_xlnet.py
@@ -16,6 +16,7 @@ import pandas as pd
 from tqdm import tqdm, trange
 import matplotlib.pyplot as plt
 import sys
+from sklearn.metrics import roc_auc_score, accuracy_score
 
 
 def main():
@@ -132,6 +133,63 @@ def create_attn_masks(input_ids):
         seq_mask = [float(i > 0) for i in seq]
         attention_masks.append(seq_mask)
     return attention_masks
+
+class XLNetForMultiLabelSequenceClassification(torch.nn.Module):
+
+    def __init__(self, num_labels=2):
+        super(XLNetForMultiLabelSequenceClassification, self).__init__()
+        self.num_labels = num_labels
+        self.xlnet = XLNetModel.from_pretrained('xlnet-base-cased')
+        self.classifier = torch.nn.Linear(768, num_labels)
+
+        torch.nn.init.xavier_normal_(self.classifier.weight)
+
+
+    def forward(self, input_ids, token_type_ids=None,
+                attention_mask=None, labels=None):
+        # last hidden layer
+        last_hidden_state = self.xlnet(input_ids=input_ids,
+                                       attention_mask=attention_mask,
+                                       token_type_ids=token_type_ids)
+        # pool the outputs into a mean vector
+        mean_last_hidden_state = self.pool_hidden_state(last_hidden_state)
+        logits = self.classifier(mean_last_hidden_state)
+
+        if labels is not None:
+            #loss_fct = BCEWithLogitsLoss()
+            loss_fct = CrossEntropyLoss()
+            # loss_fct = MultiLabelSoftMarginLoss()
+
+            # loss = loss_fct(logits.view(-1, self.num_labels),
+            #                 labels.view(-1, self.num_labels))
+
+            loss = loss_fct(logits, labels)
+            return loss
+        else:
+            return logits
+
+    def freeze_xlnet_decoder(self):
+        """
+        Freeze XLNet weight parameters. They will not be updated during training.
+        """
+        for param in self.xlnet.parameters():
+            param.requires_grad = False
+
+    def unfreeze_xlnet_decoder(self):
+        """
+        Unfreeze XLNet weight parameters. They will be updated during training.
+        """
+        for param in self.xlnet.parameters():
+            param.requires_grad = True
+
+    def pool_hidden_state(self, last_hidden_state):
+        """
+        Pool the output vectors into a single mean vector
+        """
+        last_hidden_state = last_hidden_state[0]
+        mean_last_hidden_state = torch.mean(last_hidden_state, 1)
+        return mean_last_hidden_state
+
 
 
 def roc_auc_score_FIXED(y_true, y_pred):
